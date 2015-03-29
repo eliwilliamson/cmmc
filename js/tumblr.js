9119823ff046e59b008fa0ee333b29a6783f7396
eliwilliamson <eliwilliamson@gmail.com>
2015-03-28 23:58:25 -0500

    Built cmmc from commit 6babd63 on branch master

--- js/tumblr.js
+++ js/tumblr.js
@@ -28,7 +28,7 @@ $(function() {
 
             var postElement = $('<div class="item" style="background-image: url(http://eliwilliamson.github.io/cmmc/images/landing-hero-bg.jpg);"><div class="container"><div class="caption vertical-center text-center"><h1 class="fadeInDown-1 light-color">' + post.title + '</h1><p class="fadeInDown-2 light-color">' + post.body + '</p></div><!-- /.caption --></div><!-- /.container --></div><!-- /.item -->')
 
-            postElement.appendTo('#hero owl-wrapper')
+            postElement.appendTo('#hero #owl-main')
 
         })
     }
