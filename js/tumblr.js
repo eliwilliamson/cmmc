54f466be390483b3ca9d974015e83946947c27f1
eliwilliamson <eliwilliamson@gmail.com>
2015-03-29 01:52:08 -0500

    Built cmmc from commit a353cc1 on branch master

--- js/tumblr.js
+++ js/tumblr.js
@@ -26,8 +26,9 @@ $(function() {
 
         posts.forEach(function (post) {
 
-            var initElement = $(post.body);
-            initElement.appendTo('#hero #hidden');
+            var initElement = $(post.body)
+            var instance = 1
+            initElement.appendTo('#hero #hidden' + instance)
 
             var sliderBody = $('#hidden p:first-child').text();
             var sliderLink = $('#hidden a').attr('href');
@@ -36,16 +37,11 @@ $(function() {
 
             console.log(sliderCTA);
 
-            var postSlider = $('<div class="item" style="background-image: url(' + sliderBG + ');"><div class="container"><div class="caption vertical-center text-center"><h1 class="fadeInDown-1 light-color">' + post.title + '</h1><p class="fadeInDown-2 light-color">' + sliderBody + '</p><div class="fadeInDown-3"><a href="' + sliderLink + '" class="btn btn-large">' + sliderCTA + '</a></div></div></div></div>');
+            var postSlider = $('<div class="item" style="background-image: url(' + sliderBG + ');"><div class="container"><div class="caption vertical-center text-center"><h1 class="fadeInDown-1 light-color">' + post.title + '</h1><p class="fadeInDown-2 light-color">' + sliderBody + '</p><div class="fadeInDown-3"><a href="' + sliderLink + '" class="btn btn-large">' + sliderCTA + '</a></div></div></div></div>')
 
-            postSlider.appendTo('#hero #owl-main');
+            postSlider.appendTo('#hero #owl-main')
 
-            initElement = null;
-            sliderBody = null;
-            sliderLink = null;
-            sliderCTA = null;
-            sliderBG = null;
-            postSlider = null;
+            ++instance
 
         })
     }
